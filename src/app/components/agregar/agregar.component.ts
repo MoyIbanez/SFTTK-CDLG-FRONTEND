5beb93654848d1964d309e247ee679746425bc00
Carlos <vintersorg30@hotmail.com>
2019-06-20 10:59:23 -0500

    reparado

--- src/app/components/agregar/agregar.component.ts
+++ src/app/components/agregar/agregar.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styleUrls: ['./agregar.component.css']
+  styleUrls: []
 })
 export class AgregarComponent implements OnInit {
 
